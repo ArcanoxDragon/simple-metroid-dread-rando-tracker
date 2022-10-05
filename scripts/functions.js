32689810fbc1455d0687430ec9a5756dd0704d3e
Arcanox <816612+ArcanoxDragon@users.noreply.github.com>
2022-10-04 21:31:35 -0500

    Added crude DNA counter

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,6 @@
+let dnaRequired = 0;
+let dnaAcquired = 0;
+
 function toggleItem(e) {
   e.preventDefault();
 
@@ -8,7 +11,30 @@ function toggleItem(e) {
   checkGoMode();
 };
 
+function decreaseDna(e) {
+  e.preventDefault();
+
+  dnaAcquired = Math.max(0, dnaAcquired - 1);
+  updateDnaCounter();
+  checkGoMode();
+}
+
+function increaseDna(e) {
+  e.preventDefault();
+
+  dnaAcquired = Math.min(dnaRequired, dnaAcquired + 1);
+  updateDnaCounter();
+  checkGoMode();
+}
+
+function updateDnaCounter() {
+  var dnaCurrent = document.querySelector('#current-dna-count');
+
+  dnaCurrent.innerHTML = `${dnaAcquired}/${dnaRequired}`;
+}
+
 function checkGoMode() {
+  var params = new URLSearchParams(window.location.search);
   var unacquiredItemNodes = document.querySelectorAll('.item.grayscale');
   var goModeNode = document.querySelector('#go-mode');
   var unacquiredItems = [];
@@ -28,6 +54,10 @@ function checkGoMode() {
     || unacquiredItems.includes('wavebeam')
     || unacquiredItems.includes('powerbomb')
   ) {
+    goMode = false;
+  }
+
+  if (dnaAcquired < dnaRequired) {
     goMode = false;
   }
 
